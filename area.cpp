ed8d20cf1b09cefce7137cf09c4047a742adb309
Bajpaisaumya <134991597+Bajpaisaumya@users.noreply.github.com>
2023-06-15 00:47:06 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace  std;
+int main()
+{
+    int r;
+    float pi=3.14,A,S;
+    cout<<"enter the radius of the circle";
+    cin>>r;
+    A=pi*r*r;
+    cout<<A<<endl;
+S=2*pi*r;
+cout<<S<<endl;
+
+    return 0;
+
+}
